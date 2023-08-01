8a80a8dca1067171eb3f8c9969717c55cfa60d9e
Adalberto R. Ribeiro <beto1821@uol.com.br>
2023-07-31 23:37:32 -0300

    feat: methods

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ python3 -m venv .venv && source .venv/bin/activate
 flask
 
 ### Testando no Postman
+
+Here's how you can do it in Postman:
+
+- Open Postman and go to the request builder.
+- Select the HTTP method as POST.
+- Enter the URL: http://127.0.0.1:5000/carros
+- Click on the "Headers" tab.
+- Add a new header with the key Content-Type and the value application/json.
+- Go to the "Body" tab.
+- Select the "raw" option.
+- Enter the JSON data in the request body:
--- main.py
+++ main.py
@@ -1,19 +1,66 @@
-from flask import Flask, request
+from flask import Flask, jsonify, make_response, request
 from bd import Carros
 
 app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False
 
 
-@app.route('/carros', methods=['GET'])
+@app.route("/carros", methods=["GET"])
 def get_carros():
     return Carros
 
 
-@app.route('/carros', methods=['POST'])
-def create_car():
+@app.route("/carros", methods=["POST"])
+def create_carro():
     carro = request.json
     Carros.append(carro)
-    return Carros
+    return make_response(
+        jsonify(mensagem="Carro cadastrado com sucesso!!!",
+                carro=carro,
+                Carros=Carros)
+    )
+
+
+@app.route("/carros/<int:id>", methods=["DELETE"])
+def delete_carro(id):
+    carro_to_delete = None
+    for carro in Carros:
+        if carro['id'] == id:
+            carro_to_delete = carro
+            break
+
+    if carro_to_delete:
+        Carros.remove(carro_to_delete)
+        return make_response(
+            jsonify(mensagem="Carro removido com sucesso!",
+                    carro=carro_to_delete),
+            200,
+        )
+    else:
+        return make_response(jsonify(mensagem="Carro não encontrado!",
+                                     carro=None), 404)
+
+
+@app.route("/carros/<int:id>", methods=["PATCH"])
+def update_carro(id):
+    carro_to_update = None
+    for carro in Carros:
+        if carro['id'] == id:
+            carro_to_update = carro
+            break
+
+    if carro_to_update:
+        data = request.json
+        for key, value in data.items():
+            carro_to_update[key] = value
+        return make_response(
+            jsonify(mensagem="Carro atualizado com sucesso!",
+                    carro=carro_to_update),
+            200,
+        )
+    else:
+        return make_response(jsonify(mensagem="Carro não encontrado!",
+                                     carro=None), 404)
 
 
 app.run()
